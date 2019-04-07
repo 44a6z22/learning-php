5fba6ca33e375d2a58779089bfd1e776160a4cc2
hamdaoui hamza <hhamdaoui31@gmail.com>
2019-04-07 02:46:11 +0100

    working on the database

---
+++ sql/database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE library ; 
+USE library ; 
+CREATE TABLE users(
+    userId INT PRIMARY KEY AUTO_INCREEMENT NOT NULL,
+    userUserName 
+    userFirstName VARCHAR(30), 
+    userLastName VARCHAR(30),
+)
